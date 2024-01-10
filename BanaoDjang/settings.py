0d741996518b072de46808c3ca7db0c3f3eab450
RiskiRishiii <126314803+RiskiRishiii@users.noreply.github.com>
2024-01-10 09:21:58 +0530

    Update settings.py

--- BanaoDjang/settings.py
+++ BanaoDjang/settings.py
@@ -17,10 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4or2au8)66)hy4s19e!dz$*^d1sgfeepadu@$5-z#i$fr^_hd$'
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
